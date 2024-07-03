03a09b441cacb8f4a92e535e9deb2d4815d80ae8
Edgar Jacquemoud <cookiesarenightmares@gmail.com>
2024-07-04 00:17:39 +0200

    Create window Windows

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+libs=-luser32
+warnings=-Wno-writable-strings
 
-clang src/main.cpp -oJumpAndSwap.exe
+clang src/main.cpp -oJumpAndSwap.exe $libs $warnings
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,100 @@
 
+
+
+
+
+#include <stdio.h>
+
+//########################################################################
+// PLATFORM GLOBALS
+//########################################################################
 bool static running = true;
+
+
+//########################################################################
+// PLATFORM FUNCTIONS
+//########################################################################
+bool platform_create_window(int width, int height, char* title);
+void update_platform_window();
+//########################################################################
+// WINDOWS PLATFORM
+//########################################################################
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+//########################################################################
+// WINDOWS GLOBALS
+//########################################################################
+static const char* W_MAIN_WINDOW_CLASS_NAME = "WMainWindow";
+static HWND W_MAIN_WINDOW_HANDLE;
+
+//########################################################################
+// PLATFORM IMPLEMENTATIONS
+//########################################################################
+LRESULT CALLBACK windows_window_callback(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    LRESULT result = 0;
+
+    switch (msg)
+    {
+    case WM_CLOSE:
+        running = false;
+        break;
+    case WM_KEYDOWN:
+        break;
+    default:
+        result = DefWindowProcA(window, msg, wParam, lParam);
+    }
+    return result;
+};
+
+
+bool platform_create_window(int width, int height, char* title)
+{
+    HINSTANCE instance = GetModuleHandleA(0);
+
+    WNDCLASS wc = {};
+    wc.hInstance = instance;
+    wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = W_MAIN_WINDOW_CLASS_NAME; // unique id
+    wc.lpfnWndProc = windows_window_callback; // callback for inputs in the window
+
+    if(!RegisterClass(&wc))
+    {
+        return false;
+    }
+
+    int dwStyle = WS_OVERLAPPEDWINDOW;
+
+    W_MAIN_WINDOW_HANDLE = CreateWindowExA(0, W_MAIN_WINDOW_CLASS_NAME, title, dwStyle,100,100, width, height, NULL,NULL, instance, NULL);
+    if(W_MAIN_WINDOW_HANDLE == NULL)
+    {
+        return false;
+    }
+
+    ShowWindow(W_MAIN_WINDOW_HANDLE, SW_SHOW);
+    return true;
+}
+void update_platform_window()
+{
+    MSG msg;
+    while(PeekMessageA(&msg,W_MAIN_WINDOW_HANDLE, 0,0,PM_REMOVE ))
+    {
+        TranslateMessage(&msg);
+        DispatchMessageA(&msg);
+    }
+}
+
+#endif
 
 int main()
 {
+    platform_create_window(1200,900, "hello world");
     while (running)
     {
-        /* code */
+        update_platform_window();
     }
     return 0;
 }
