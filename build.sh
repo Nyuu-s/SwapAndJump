b3fae94c53452a4440c44df64953d7f28b424bd6
Edgar Jacquemoud <59689456+Nyuu-s@users.noreply.github.com>
2024-07-08 23:11:01 +0200

    wrong build target

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 libs=-luser32
 warnings="-Wno-writable-strings -Wno-format-security"
 
-clang++ -g src/main.cpp -oJumpAndSwap.exe $libs $warnings --target=x86_64-w64-windows-gnu
+clang++ -g src/main.cpp -oJumpAndSwap.exe $libs $warnings 
